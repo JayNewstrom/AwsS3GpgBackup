616e14791c6af70426a231134920998fa78ee1b4
Jay Newstrom <jaynewstrom@gmail.com>
2021-06-24 12:06:45 -0500

    Add support for tar.gz compression.

--- root/app/restore.sh
+++ root/app/restore.sh
@@ -5,6 +5,7 @@ FILENAME=${2:?"Please specify the file you'd like to restore."}
 
 # Configurable Paramaters - via environment variables.
 AWS_OPTIONS=${AWS_OPTIONS:-""}
+COMPRESSION_TYPE=${COMPRESSION_TYPE:-"zip"}
 export GNUPGHOME=${GNUPGHOME:-"/app/.gnupg"}
 export AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE:-"/config/.aws/credentials"}
 export AWS_CONFIG_FILE=${AWS_CONFIG_FILE:-"/config/.aws/config"}
@@ -18,6 +19,13 @@ ZIP_FILENAME=${FILENAME%.gpg}
 
 gpg --output "/zip/$ZIP_FILENAME" --decrypt "/gpg/$FILENAME"
 
-unzip "/zip/$ZIP_FILENAME" -d /restore/
+if [ "$COMPRESSION_TYPE" = "zip" ]; then
+  unzip "/zip/$ZIP_FILENAME" -d /restore/
+elif [ "$COMPRESSION_TYPE" = "tar.gz" ]; then
+  tar -xzvf "/zip/$ZIP_FILENAME" -C /restore/
+else
+  echo "COMPRESSION_TYPE not supported."
+  exit 1
+fi
 
 rm "/gpg/$FILENAME" "/zip/$ZIP_FILENAME"
